8946c67101eefa8215d74eaed4a016984b35908a
Gyananjay31 <gyananjaydwivedi@gmail.com>
2025-04-21 18:35:22 +0530

    commiting the changes

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App.jsx'
-import Main from "./todoList/MAin.jsx"
+import Main from "./todoList/Main.jsx"
 
 createRoot(document.getElementById('root')).render(
   <Main />
